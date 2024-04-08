a1bf80e7e4db20478bd94185ecfd404eb07e190b
laura-bvc <156967330+laura-bvc@users.noreply.github.com>
2024-04-07 22:16:49 -0600

    V1

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-const express = require('express');
+const express = require('express')
 const PORT = 8080;
-const app = express();
+const app = express()
+const userRoutes = require('./routes/userRoute')
+// convert form data to javascript object and put into request body
+app.use(express.urlencoded({extended: false}))
 
-app.use(express.json());
-
-
-app.get('/tshirt', (req, res) => {
-	res.status(200).send({
-		tshirt: 'Tee',
-		size: 'large'
-	});
-});
-
-app.post('/tshirt/:id', (req, res) => {
-	const {id} = req.params;
-	const {logo} = req.body;
-	
-	if (!logo) {
-	res.status(418).send( { message: 'We need a logo!!'});
-	}
-	res.send({
-		tshirt: `Tshirt with your logo ${logo} and ID of ${id}`
-	});
-});
-
+// convert json to javascript object and put into request body
+app.use(express.json())
+app.use(userRoutes)
 
 app.listen(PORT, ()=>{
     console.log(`server is running at ${PORT}........`)
