d24a7773916a639abc0f521f418f246fb119a12d
laura-bvc <156967330+laura-bvc@users.noreply.github.com>
2024-04-07 22:52:03 -0600

    Update for CORS policy

--- app.js
+++ app.js
@@ -7,6 +7,15 @@ app.use(express.urlencoded({extended: false}))
 
 // convert json to javascript object and put into request body
 app.use(express.json())
+
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    next();
+})
+
+
 app.use(userRoutes)
 
 app.listen(PORT, ()=>{
